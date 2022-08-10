ce580893b736f69fe5513a3adcc8d4b8fd4f6e03
Wallison Ferreira <contato.sanfer@gmail.com>
2022-08-10 15:46:33 -0500

    #5 Corrigir a operação de divisão

--- calculadora.py
+++ calculadora.py
@@ -6,13 +6,13 @@ print('{} + {} = '.format(number_1, number_2))
 print(number_1 + number_2)
 
 # Subtraction
-print('{} + {} = '.format(number_1, number_2))
+print('{} - {} = '.format(number_1, number_2))
 print(number_1 - number_2)
 
 # Multiplication
-print('{} + {} = '.format(number_1, number_2))
-print(number_1 - number_2)
+print('{} * {} = '.format(number_1, number_2))
+print(number_1 * number_2)
 
 # Division
-print('{} + {} = '.format(number_1, number_2))
-print(number_1 - number_2)
+print('{} / {} = '.format(number_1, number_2))
+print(number_1 / number_2)
