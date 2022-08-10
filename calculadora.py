bbc50cff3339c80a091c79707bc9ece2239b4aa0
Wallison Ferreira <contato.sanfer@gmail.com>
2022-08-10 16:12:56 -0500

    #4 Permitir que o usuário realize novo cálculo

--- calculadora.py
+++ calculadora.py
@@ -1,41 +1,61 @@
-operation = input('''
-Please type in the math operation you would like to complete:
-+ for addition
-- for substraction
-* for multiplication
-/ for division
-''')
+def calculate():
+    operation = input('''
+    Please type in the math operation you would like to complete:
+    + for addition
+    - for substraction
+    * for multiplication
+    / for division
+    ''')
 
-number_1 = int(input('Enter yout first number: '))
-number_2 = int(input('Enter yout first number: '))
+    number_1 = int(input('Enter yout first number: '))
+    number_2 = int(input('Enter yout first number: '))
 
-# Sum
-def invokeSum(number_1, number_2):
-    print('{} + {} = '.format(number_1, number_2))
-    print(number_1 + number_2)
+    # Sum
+    def invokeSum(number_1, number_2):
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
 
-# Subtraction
-def invokeSub(number_1, number_2):
-    print('{} - {} = '.format(number_1, number_2))
-    print(number_1 - number_2)
+    # Subtraction
+    def invokeSub(number_1, number_2):
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
 
-# Multiplication
-def invokeMult(number_1, number_2):
-    print('{} * {} = '.format(number_1, number_2))
-    print(number_1 * number_2)
+    # Multiplication
+    def invokeMult(number_1, number_2):
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
 
-# Division
-def invokeDiv(number_1, number_2):
-    print('{} / {} = '.format(number_1, number_2))
-    print(number_1 / number_2)
+    # Division
+    def invokeDiv(number_1, number_2):
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
 
-if operation == '+':
-    invokeSum(number_1, number_2)
-elif operation == '-':
-    invokeSub(number_1, number_2)
-elif operation == '*':
-    invokeMult(number_1, number_2)
-elif operation == '/':
-    invokeDiv(number_1, number_2)
-else:
-    print('You have not typed a valid operator, please run the program again.')
+    if operation == '+':
+        invokeSum(number_1, number_2)
+        again()
+    elif operation == '-':
+        invokeSub(number_1, number_2)
+        again()
+    elif operation == '*':
+        invokeMult(number_1, number_2)
+        again()
+    elif operation == '/':
+        invokeDiv(number_1, number_2)
+        again()
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+        again()
+
+def again():
+    calc_again = input('''
+    Do you want to calculate again?
+    ''')
+
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+    else:
+        again()
+
+calculate()
